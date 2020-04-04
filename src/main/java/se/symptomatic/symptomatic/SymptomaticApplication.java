1b45a5065a0e578bd98d0a1c82c6586b207053b8
Steven Hu <husteven.91@gmail.com>
2020-04-04 11:45:43 +0200

    Setup project

---
+++ src/main/java/se/symptomatic/symptomatic/SymptomaticApplication.java
@@ -0,0 +1,13 @@
+package se.symptomatic.symptomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptomaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymptomaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/symptomatic/symptomatic/SymptomaticApplicationTests.java
@@ -0,0 +1,13 @@
+package se.symptomatic.symptomatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymptomaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
