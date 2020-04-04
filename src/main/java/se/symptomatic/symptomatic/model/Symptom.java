36263355ab1339d3e3ad6afe5e7a3f974f5d0149
Steven Hu <husteven.91@gmail.com>
2020-04-04 22:17:21 +0200

    update equal check for Symptom

--- src/main/java/se/symptomatic/symptomatic/model/Symptom.java
+++ src/main/java/se/symptomatic/symptomatic/model/Symptom.java
@@ -1,15 +1,29 @@
 package se.symptomatic.symptomatic.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import java.util.Objects;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 public class Symptom {
     long id;
     String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Symptom symptom = (Symptom) o;
+        return id == symptom.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
