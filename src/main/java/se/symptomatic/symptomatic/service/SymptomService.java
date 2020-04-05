9ae105919b161dc051e16e092b36f9f0b2c87baf
Steven Hu <husteven.91@gmail.com>
2020-04-05 10:25:14 +0200

    update symptom percentage

--- src/main/java/se/symptomatic/symptomatic/service/SymptomService.java
+++ src/main/java/se/symptomatic/symptomatic/service/SymptomService.java
@@ -1,6 +1,7 @@
 package se.symptomatic.symptomatic.service;
 
 import org.springframework.stereotype.Service;
+import se.symptomatic.symptomatic.exceptions.NotFoundException;
 import se.symptomatic.symptomatic.model.Symptom;
 
 import java.util.ArrayList;
@@ -12,13 +13,27 @@ public class SymptomService {
         ArrayList<Symptom> symptoms = new ArrayList<>();
         symptoms.add(new Symptom(1,"Fever"));
         symptoms.add(new Symptom(2,"Cough"));
-        symptoms.add(new Symptom(3,"Shortness of breath"));
-        symptoms.add(new Symptom(4,"Fatigue"));
-        symptoms.add(new Symptom(5,"Sore throat"));
+        symptoms.add(new Symptom(3,"Difficulty breathing"));
+        symptoms.add(new Symptom(4,"Tiredness"));
+        symptoms.add(new Symptom(5,"Chest tightness"));
         return symptoms;
     }
 
     public double getSymptomPercentage(long id){
-        return (id * 67) % 100;
+        switch ((int)id){
+            case 1:
+                return 93.0;
+            case 2:
+                return 70.0;
+            case 3:
+                return 31.0;
+            case 4:
+                return 50.0;
+            case 5:
+                return 29.0;
+            default:
+                throw new NotFoundException("Symptom " +id + " not found");
+        }
+
     }
 }
